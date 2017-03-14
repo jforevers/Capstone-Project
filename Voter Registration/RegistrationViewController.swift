9a62a963b56e6f8fab47fb65862b63f5fa345be4
jbekcc <jbekcc@mail.missouri.edu>
2017-03-14 14:39:00 -0500

    More logic added

--- Voter Registration/RegistrationViewController.swift
+++ Voter Registration/RegistrationViewController.swift
@@ -18,6 +18,29 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var ageSwitch: UISwitch!
     @IBOutlet weak var firstPageError: UILabel!
     
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var middleName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var suffix: UITextField!
+    @IBOutlet weak var maleSwitch: UISwitch!
+    @IBOutlet weak var femaleSwitch: UISwitch!
+    
+    func checkName() -> Bool {
+        if(firstName.text!.isEmpty || middleName.text!.isEmpty || lastName.text!.isEmpty || (maleSwitch.isOn && femaleSwitch.isOn) || (!maleSwitch.isOn && !femaleSwitch.isOn))
+        {
+            return false
+        }
+        return true
+    }
+    
+    func toNameAndAge() -> Bool {
+        if(!Citizen_switch.isOn || !residentSwitch.isOn || !ageSwitch.isOn)
+        {
+            displayError(errorText: firstPageError, errorMessage: "Must be a citizen, resident, and 18 before election day to register")
+            return false;
+        }
+        return true;
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,20 +64,9 @@ class RegistrationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    // MARK: - Navigation
-
-    func toNameAndAge() -> Bool {
-        if(!Citizen_switch.isOn || !residentSwitch.isOn || !ageSwitch.isOn)
-        {
-            displayError(errorText: firstPageError, errorMessage: "Must be a citizen, resident, and 18 before election day to register")
-            return false;
-        }
-        return true;
-     }
-     
-     
+    
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
